85eadbc8f490023acdde540ae6fb69c15e87c7f6
unknown <rayas143120@gmail.com>
2019-06-19 14:17:59 +0700

    update

--- testing.js
+++ testing.js
@@ -174,8 +174,74 @@
 // Profile.findAll().then(data=>{console.log(data)})
 
 
-var role = '{\"conditions\":{\"any\":[{\"fact\":\"outlet_1_voltage_3_PDU\",\"operator\":\"equal\",\"value\":5000,\"value2\":\"\"}]},\"event\":{\"type\":1,\"params\":{\"message\":\"Player has fouled out!\",\"action_id\":1}}}'
+// var role = '{\"conditions\":{\"any\":[{\"fact\":\"outlet_1_voltage_3_PDU\",\"operator\":\"equal\",\"value\":5000,\"value2\":\"\"}]},\"event\":{\"type\":1,\"params\":{\"message\":\"Player has fouled out!\",\"action_id\":1}}}'
 
 
-var tt = JSON.parse(role)
-console.log(tt.conditions)
+// var tt = JSON.parse(role)
+// console.log(tt.conditions)
+
+
+
+
+let Engine = require('../role-js/node_modules/json-rules-engine/dist').Engine
+ 
+/**
+ * Setup a new engine
+ */
+let engine = new Engine()
+ 
+// define a rule for detecting the player has exceeded foul limits.  Foul out any player who:
+// (has committed 5 fouls AND game is 40 minutes) OR (has committed 6 fouls AND game is 48 minutes)
+engine.addRule({
+  conditions: {
+    any: [{
+      all: [{
+        fact: 'gameDuration',
+        operator: 'equal',
+        value: 40
+      }, {
+        fact: 'personalFoulCount',
+        operator: 'greaterThanInclusive',
+        value: 5
+      }]
+    }, {
+      all: [{
+        fact: 'gameDuration',
+        operator: 'equal',
+        value: 48
+      }, {
+        fact: 'personalFoulCount',
+        operator: 'greaterThanInclusive',
+        value: 6
+      }]
+    }]
+  },
+  event: {  // define the event to fire when the conditions evaluate truthy
+    type: 'fouledOut',
+    params: {
+      message: 'Player has fouled out!'
+    }
+  }
+})
+ 
+/**
+ * Define facts the engine will use to evaluate the conditions above.
+ * Facts may also be loaded asynchronously at runtime; see the advanced example below
+ */
+let facts = {
+  personalFoulCount: 6,
+  gameDuration: 40
+}
+ 
+// Run the engine to evaluate
+engine
+  .run(facts)
+  .then(events => { // run() returns events with truthy conditions
+    events.map(event => console.log(event.params.message))
+  })
+ 
+/*
+ * Output:
+ *
+ * Player has fouled out!
+ */
