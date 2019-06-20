c4d1e55e973070ab45b1572225e52f8c71e9a2d2
unknown <rayas143120@gmail.com>
2019-06-20 10:40:20 +0700

    update

--- server.js
+++ server.js
@@ -205,3 +205,66 @@ Action1.findAll({
     }
 })
 
+
+
+
+
+///Role
+
+// const engine = require('./role1.js')
+// const databas = require('./fromdatabase.js')
+
+var role = db.role_backend;
+
+
+// engine.createRule()
+
+
+const fs = require('fs');
+
+let dataFact = fs.readFileSync('allFact.json');
+let bisa = JSON.parse(dataFact);
+
+
+
+
+
+
+
+let Engine = require('../role-js/node_modules/json-rules-engine/dist').Engine;
+let engine = new Engine()
+
+
+// exports.facts = ()=>{
+
+// }
+role.findAll({attributes : ['roleallcondition']}).then(a=>{
+    a.map(data=> {
+        // console.log(data.roleallcondition.conditions)
+        var aw = JSON.parse(data.roleallcondition)
+        // console.log(data.roleallcondition)
+        // console.log(aw.conditions.all)
+        // var arr= []
+        // arr.push(aw)
+
+        engine.addRule(aw)
+        
+
+
+        let facts = 
+            {
+                humidity_5_PDU : 112,
+                humidity_4_PDU: 12,
+                load_3_UPS : 12,
+                inlet_power_5_PDU : 0,
+                outlet_1_current_5_PDU : 0,
+                temperature_5_PDU : 23
+            }
+
+
+        engine.run(bisa).then(events => {
+                events.map(event => console.log(event.params.action_id))
+        }).catch(console.log)
+    })
+
+})
